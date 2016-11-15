4cfc5436c49060f0bcf34bf861c133077da19049
FransMeerhoff <F.A.M.Meerhoff@uva.nl>
2016-11-15 15:00:52 +0100

    Added Comment

--- ModelView/dialog.cpp
+++ ModelView/dialog.cpp
@@ -7,7 +7,10 @@ Dialog::Dialog(QWidget *parent) :
 {
 	ui->setupUi(this);
 	QStringList list;
+
+	//Below is the list
 	list << "aap" << "noten" << "mies" << "toto" << "tata!" << "blabla";
+
 	_model = new QStringListModel(this);
 	_model->setStringList(list);
 	ui->comboBox->setModel(_model);
